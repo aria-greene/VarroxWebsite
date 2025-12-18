3becc869f7f9fb8cce895ca6188778ec258a61da
Varrox <140557931+Varrox@users.noreply.github.com>
2025-12-18 11:52:59 -0500

    stuff

---
+++ quick-links.js
@@ -0,0 +1,19 @@
+const links = {
+    "godot" : "https://godotengine.org",
+    "youtube" : "https://youtube.com/@Varrox",
+    "github" : "https://github.com/varrox",
+    "bluesky" : "https://bluesky.com/"
+}
+
+function apply_links()
+{
+    for(var i = 0; i < links.length; i++)
+    {
+        var elements = document.getElementsByClassName(links.keys[i])
+        for(var j = 0; j < elements.length; j++)
+        {
+            elements[j].
+        }
+    }
+    //
+}
